e53649e559dff78c3cc025b170d36c12616dc77b
Asim Mahar <amahar@RESAMAHAR.home>
2016-04-24 20:40:12 -0400

    changes

--- practice.js
+++ practice.js
@@ -1,34 +1,38 @@
-var str = 'petsm artm dshop';
 
-function run(str){
+var str = "hello world";
+function letter(str) {
 
-// var rev = '';
-// var word = '';
-// for (i=0; i<str.length; i++) {
-// 	rev = rev + str.charAt(i);
+	eachnumber = '';
+	eachchar = '';
 
-		
-// 		//var j = 0;
-// 		if (str[i] === ' '){
+	for (i=0; i < str.length; i++) {
 
-// 			word = rev;
-// 			return word;
-// 		}
+		eachnumber = (str.charCodeAt(i)+1);
+		if(eachnumber == '123'){
+			eachnumber = '97';
+		}
+		eachchar = eachchar + String.fromCharCode(eachnumber);
+		// eachchar = eachchar.replace(/z/g,'a');
 
-// }
-
-  str = str.split(' ');
-
-  return str; 
 	
-// return rev;
+	}
 
+	eachchar = eachchar.replace(/a/g,'A').replace(/e/g,'E')
+				.replace(/i/g,'I').replace(/o/g,'O').replace(/u/g,'U')
+				.replace(/!/g,' ');
+				;
+	// for (j=0; j < eachletter.length; j++) {
+
+	// 		eachchar = eachchar + String.fromCharCode(eachletter);
+	// }
+
+
+	console.log(typeof(eachnumber));
+	console.log(typeof(eachchar));
+	return eachchar;
 }
 
-alert(run(str)); // returns dm tramstep
-
-
-
+console.log(letter(str));
 
 
 
